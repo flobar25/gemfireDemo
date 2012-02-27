c23fc7cea9da3a0bceae501c37dc1774156ebf82
Florent Bariod <fbariod@gmail.com>
2012-02-27 16:08:37 -0500

    moved user service and removed helloworld

---
+++ src/main/java/com/arvatosystems/services/UsersService.java
@@ -0,0 +1,28 @@
+package com.arvatosystems.services;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.gemfire.GemfireTemplate;
+import org.springframework.stereotype.Component;
+
+import com.arvatosystems.dto.UserDTO;
+
+@Component
+public class UsersService {
+
+	@Resource
+	private GemfireTemplate usersStorageClient;
+	
+	public void insertUser(){
+		
+	}
+	
+
+	public UserDTO putInCache(String key, UserDTO value){
+		return usersStorageClient.put(key, value);
+	}
+	
+	public UserDTO getFromCache(String key){
+		return usersStorageClient.get(key);
+	}
+}
